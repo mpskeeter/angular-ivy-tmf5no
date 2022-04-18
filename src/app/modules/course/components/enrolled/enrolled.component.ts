6f4949eda90331c508140cbba194f3f8180bdd9c
Mark Peters <mpskeeter@gmail.com>
2022-04-18 10:19:44 -0400

    updates

--- src/app/modules/course/components/enrolled/enrolled.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-enrolled',
-  templateUrl: './enrolled.component.html',
-  styleUrls: ['./enrolled.component.scss'],
-})
-export class EnrolledComponent {}
