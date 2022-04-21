b8c596807b5cb04c574536019e59b88169f7e78c
Mark Peters <mpskeeter@gmail.com>
2022-04-21 07:46:43 -0400

    updates

--- src/app/modules/admin/course-admin/components/course-edit/course-edit.component.ts
+++ src/app/modules/admin/course-admin/components/course-edit/course-edit.component.ts
@@ -23,125 +23,10 @@ export class CourseEditComponent implements OnInit, OnDestroy {
   // form = new FormGroup({});
   model: Course = {};
   options: FormlyFormOptions = {};
-  fields: FormlyFieldConfig[] = [
-    // id: [record?.id || null],
-    {
-      key: 'id',
-      type: 'input',
-      hideExpression: 'true',
-    },
+  fields: FormlyFieldConfig[] = [];
 
-    // name: [record?.name || null],
-    {
-      key: 'name',
-      type: 'input',
-      templateOptions: {
-        type: 'text',
-        label: 'Name',
-        placeholder: 'Name',
-        required: true,
-      },
-    },
-
-    // playlistId: [record?.playlistId || null]
-    {
-      key: 'playlistId',
-      type: 'select',
-      templateOptions: {
-        label: 'Playlist',
-        options: this.playlistService.items$
-          .pipe(
-            map((item: Partial<PlayList>) => {
-              return { label: item.name, value: item.id };
-            }),
-          )
-          .subscribe((items) => items),
-      },
-    },
-
-    // subject: [record?.subject || null],
-    {
-      key: 'subject',
-      type: 'input',
-      templateOptions: {
-        type: 'text',
-        label: 'Subject',
-        placeholder: 'Subject',
-      },
-    },
-
-    // description: [record?.description || null],
-    {
-      key: 'description',
-      type: 'input',
-      templateOptions: {
-        type: 'text',
-        label: 'Description',
-        placeholder: 'Description',
-      },
-    },
-
-    // statusId: [record?.statusId || null],
-    {
-      key: 'statusId',
-      type: 'select',
-      templateOptions: {
-        label: 'Status',
-        options: this.statusService.items$
-          .pipe(
-            map((item: Partial<Status>) => {
-              return { label: item.name, value: item.id };
-            }),
-          )
-          .subscribe((items) => items),
-      },
-    },
-
-    // duration: [record?.duration || null],
-    {
-      key: 'duration',
-      type: 'input',
-      templateOptions: {
-        type: 'number',
-        label: 'Duration',
-        placeholder: 'Duration',
-      },
-    },
-
-    // datePublished: [convertDate(record?.datePublished) || null],
-    {
-      key: 'datePublished',
-      type: 'input',
-      templateOptions: {
-        type: 'date',
-        label: 'Published',
-      },
-    },
-
-    // dateUpdated: [convertDate(record?.dateUpdated) || null],
-    {
-      key: 'dateUpdated',
-      type: 'input',
-      templateOptions: {
-        type: 'date',
-        label: 'Updated',
-      },
-    },
-
-    // rating: [record?.rating || null],
-    {
-      key: 'rating',
-      type: 'input',
-      templateOptions: {
-        type: 'number',
-        label: 'Rating',
-      },
-    },
-
-    // image: [record?.image || null],
-    // statusId: [record?.statusId || null],
-    // provider: [record?.provider || null],
-  ];
+  playlists = [];
+  status = [];
 
   destroy$: Subject<boolean> = new Subject<boolean>();
 
@@ -159,11 +44,142 @@ export class CourseEditComponent implements OnInit, OnDestroy {
       .subscribe((item: Course) => {
         this.model = item;
       });
+
+    this.playlistService.items$
+      .pipe(
+        map((item: Partial<PlayList>) => {
+          return { label: item.name, value: item.id };
+        }),
+      )
+      .subscribe((items) => {
+        this.playlists = items;
+      });
+
+    this.statusService.items$
+      .pipe(
+        map((item: Partial<Status>) => {
+          return { label: item.name, value: item.id };
+        }),
+      )
+      .subscribe((items) => {
+        this.status = items;
+      });
+
+    this.loadFields();
   }
 
   ngOnDestroy() {
     this.destroy$.next(true);
     this.destroy$.complete();
+  }
+
+  loadFields() {
+    this.fields = [
+      // id: [record?.id || null],
+      {
+        key: 'id',
+        type: 'input',
+        hideExpression: 'true',
+      },
+
+      // name: [record?.name || null],
+      {
+        key: 'name',
+        type: 'input',
+        templateOptions: {
+          type: 'text',
+          label: 'Name',
+          placeholder: 'Name',
+          required: true,
+        },
+      },
+
+      // playlistId: [record?.playlistId || null]
+      {
+        key: 'playlistId',
+        type: 'select',
+        templateOptions: {
+          label: 'Playlist',
+          options: this.playlists,
+        },
+      },
+
+      // subject: [record?.subject || null],
+      {
+        key: 'subject',
+        type: 'input',
+        templateOptions: {
+          type: 'text',
+          label: 'Subject',
+          placeholder: 'Subject',
+        },
+      },
+
+      // description: [record?.description || null],
+      {
+        key: 'description',
+        type: 'input',
+        templateOptions: {
+          type: 'text',
+          label: 'Description',
+          placeholder: 'Description',
+        },
+      },
+
+      // statusId: [record?.statusId || null],
+      {
+        key: 'statusId',
+        type: 'select',
+        templateOptions: {
+          label: 'Status',
+          options: this.status,
+        },
+      },
+
+      // duration: [record?.duration || null],
+      {
+        key: 'duration',
+        type: 'input',
+        templateOptions: {
+          type: 'number',
+          label: 'Duration',
+          placeholder: 'Duration',
+        },
+      },
+
+      // datePublished: [convertDate(record?.datePublished) || null],
+      {
+        key: 'datePublished',
+        type: 'input',
+        templateOptions: {
+          type: 'date',
+          label: 'Published',
+        },
+      },
+
+      // dateUpdated: [convertDate(record?.dateUpdated) || null],
+      {
+        key: 'dateUpdated',
+        type: 'input',
+        templateOptions: {
+          type: 'date',
+          label: 'Updated',
+        },
+      },
+
+      // rating: [record?.rating || null],
+      {
+        key: 'rating',
+        type: 'input',
+        templateOptions: {
+          type: 'number',
+          label: 'Rating',
+        },
+      },
+
+      // image: [record?.image || null],
+      // provider: [record?.provider || null],
+    ];
   }
 
   close() {
