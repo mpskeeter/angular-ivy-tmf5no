7df0a6ec19097dd9add6f94021cbe7a7d1eb3258
Mark Peters <mpskeeter@gmail.com>
2022-04-26 13:53:13 -0400

    chore: user-role

--- src/app/modules/admin/role-admin/components/index.ts
+++ src/app/modules/admin/role-admin/components/index.ts
@@ -1,7 +1,7 @@
 import { RoleEditComponent } from './role-edit';
 import { RoleTableComponent } from './role-table';
 import { LayoutComponent } from './layout';
-// import { UserRoleComponent } from './user-role';
+import { UserRoleComponent } from './user-role';
 
 export const ComponentsExport = [];
 
@@ -9,11 +9,11 @@ export const Components = [
   ...ComponentsExport,
   RoleEditComponent,
   RoleTableComponent,
-  // UserRoleComponent,
+  UserRoleComponent,
   LayoutComponent,
 ];
 
 export * from './role-edit';
 export * from './role-table';
 export * from './layout';
-// export * from './user-role';
+export * from './user-role';
