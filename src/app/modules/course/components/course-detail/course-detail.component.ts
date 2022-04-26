b3d471846446ea5388fbfff91b4f7709c274dbd8
Mark Peters <mpskeeter@gmail.com>
2022-04-26 11:44:57 -0400

    chore: updating course-card and course-detail

--- src/app/modules/course/components/course-detail/course-detail.component.ts
+++ src/app/modules/course/components/course-detail/course-detail.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, ParamMap } from '@angular/router';
-import { CourseService } from '../../../shared';
-import { Course } from '../../../shared-types';
+import { BehaviorSubject, combineLatest, Observable, Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
+import { AuthenticatedUserService, CourseService } from '../../../shared';
+import { Course, Enrollment } from '../../../shared-types';
 
 @Component({
   selector: 'app-course-detail',
   templateUrl: './course-detail.component.html',
 })
-export class CourseDetailComponent implements OnInit {
+export class CourseDetailComponent implements OnInit, OnDestroy {
+  destroy$: Subject<boolean> = new Subject<boolean>();
+
+  #currentEnrollment: BehaviorSubject<Partial<Enrollment>> =
+    new BehaviorSubject<Partial<Enrollment>>(null);
+  currentEnrollment$: Observable<Partial<Enrollment>> =
+    this.#currentEnrollment.asObservable();
+
   constructor(
     public service: CourseService,
+    public user: AuthenticatedUserService,
     public activatedRoute: ActivatedRoute,
-    public router: Router,
+    public router: Router
   ) {}
 
   ngOnInit() {
@@ -21,14 +31,49 @@ export class CourseDetailComponent implements OnInit {
         this.service.get(parseInt(id, 10));
       }
     });
+
+    combineLatest([this.user.item$, this.service.item$])
+      .pipe(
+        map(([user, course]) => {
+          let current: Partial<Enrollment> = user.enrollments.find(
+            (enrollment) => enrollment.courseId === course.id
+          );
+
+          current = {
+            ...(current
+              ? current
+              : {
+                  courseId: course.id,
+                  userId: user.id,
+                }),
+            currentlyEnrolled: current ? true : false,
+          };
+          this.#currentEnrollment.next(current);
+        }),
+        takeUntil(this.destroy$)
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
   }
 
   // TODO: Need to add a check to see if the user is enrolled in the course.
-  launch(course: Partial<Course>) {
+  launchCourse(currentEnrollment: Partial<Enrollment>) {
     const url = this.router.serializeUrl(
-      this.router.createUrlTree(['/course/launch', course.id]),
+      this.router.createUrlTree(['/course/launch', currentEnrollment.courseId])
     );
     const windowFeatures = 'popup,left=100,top=100,width=920,height=920';
     window.open(url, '_blank', windowFeatures);
+  }
+
+  unAssignCourse(enrollment: Partial<Enrollment>) {
+    this.user.unenroll(enrollment);
+  }
+
+  assignCourse(enrollment: Partial<Enrollment>) {
+    this.user.enroll(enrollment);
   }
 }
