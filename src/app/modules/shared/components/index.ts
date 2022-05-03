116b25a280216def77d8ca1bb6a7d177039ade2b
Mark Peters <mpskeeter@gmail.com>
2022-05-03 07:44:11 -0400

    chore: adding new icons

--- src/app/modules/shared/components/index.ts
+++ src/app/modules/shared/components/index.ts
@@ -1,4 +1,5 @@
 import { BellIconComponent } from './bell-icon';
+import { CheckIconComponent } from './check-icon';
 import { ChipComponent } from './chip';
 import { DialogComponent } from './dialog';
 import { HeaderButtonComponent } from './header-button';
@@ -11,9 +12,11 @@ import { ToggleComponent } from './toggle';
 import { TrashCanComponent } from './trash-can';
 import { UserAvatarComponent } from './user-avatar';
 import { WatchedIconComponent } from './watched-icon';
+import { XIconComponent } from './x-icon';
 
 export const ComponentsExport = [
   BellIconComponent,
+  CheckIconComponent,
   ChipComponent,
   DialogComponent,
   HeaderButtonComponent,
@@ -26,6 +29,7 @@ export const ComponentsExport = [
   TrashCanComponent,
   UserAvatarComponent,
   WatchedIconComponent,
+  XIconComponent,
 ];
 
 export const Components = [...ComponentsExport];
