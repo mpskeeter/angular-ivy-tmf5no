ea652a9773b5b0b6e127878511bf567407b9d96f
Mark Peters <mpskeeter@gmail.com>
2022-05-06 15:51:55 -0400

    chore: changing to single button

--- src/app/modules/carousel/carousel.module.ts
+++ src/app/modules/carousel/carousel.module.ts
@@ -2,12 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { CarouselComponent } from './carousel.component';
-import { CarouselPreviousButtonComponent } from './carousel-previous-button.component';
+import { CarouselButtonComponent } from './carousel-button.component';
 import { CarouselSlideDirective } from './slide.directive';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [CarouselPreviousButtonComponent, CarouselComponent, CarouselSlideDirective],
+  declarations: [CarouselButtonComponent, CarouselComponent, CarouselSlideDirective],
   exports: [CarouselComponent, CarouselSlideDirective],
 })
 export class CarouselModule {}
