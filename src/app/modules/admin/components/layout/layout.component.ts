ed2ca09d59ab5c63e107764d4bd370ed442efaa5
Mark Peters <mpskeeter@gmail.com>
2022-05-10 10:52:55 -0400

    chore: more updates

--- src/app/modules/admin/components/layout/layout.component.ts
+++ src/app/modules/admin/components/layout/layout.component.ts
@@ -4,7 +4,6 @@ import { MenuService } from '../../../menu';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent {
   constructor(public service: MenuService) {}
