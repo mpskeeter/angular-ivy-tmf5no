19a0de84f77b0a3fd2d90b4fe8d2e62bccfcefd4
Mark Peters <mpskeeter@gmail.com>
2022-05-10 10:54:15 -0400

    chore: update bottom-nav to be more responsive

--- src/app/modules/layout/components/bottom-nav/bottom-nav.component.ts
+++ src/app/modules/layout/components/bottom-nav/bottom-nav.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-bottom-nav',
     templateUrl: './bottom-nav.component.html',
-    // styleUrls: ['./bottom-nav.component.scss'],
 })
 export class BottomNavComponent {
     footerItems = [
