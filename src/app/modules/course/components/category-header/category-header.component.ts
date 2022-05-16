b0d7c9b911b908867481a882ab4668119e9022b8
Mark Peters <mpskeeter@gmail.com>
2022-05-16 14:00:32 -0400

    chore: trying to use single component to display category

---
+++ src/app/modules/course/components/category-header/category-header.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Category } from '../../../shared-types';
+
+@Component({
+  selector: 'app-category-header',
+  templateUrl: './category-header.component.html',
+})
+export class CategoryPanelComponent {
+  @Input() item: Partial<Category> = {};
+  // @Input() allowCategoryNavigate: boolean = false;
+}
