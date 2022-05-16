afe41e5fe089e7c4ce81c9f8be6949a2d115285f
Mark Peters <mpskeeter@gmail.com>
2022-05-16 12:38:33 -0400

    chore: add left-side-modal component

--- src/app/modules/modal/components/index.ts
+++ src/app/modules/modal/components/index.ts
@@ -1,5 +1,6 @@
+import { LeftSideModalComponent } from './left-side-modal';
 import { RightSideModalComponent } from './right-side-modal';
 
-export const ComponentsExport = [RightSideModalComponent];
+export const ComponentsExport = [RightSideModalComponent. LeftSideModalComponent];
 
 export const Components = [...ComponentsExport];
