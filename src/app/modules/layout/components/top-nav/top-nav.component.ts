1f677f677761c7860aeb4d4317b392d1ca2407fc
mpskeeter <mpskeeter@gmail.com>
2022-05-16 12:12:01 -0400

    3 files modified

--- src/app/modules/layout/components/top-nav/top-nav.component.ts
+++ src/app/modules/layout/components/top-nav/top-nav.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 import { MenuService } from '../../../menu';
 
 @Component({
-	selector: 'app-top-nav',
-	templateUrl: './top-nav.component.html',
+  selector: 'app-top-nav',
+  templateUrl: './top-nav.component.html',
 })
 export class TopNavComponent {
-	constructor(public menu: MenuService) {}
+  open: boolean = false;
+
+  constructor(public menu: MenuService) {}
 }
--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared';
+import { ModalModule } from '../modal';
 import { Components, ComponentsExport } from './components';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, SharedModule],
+  imports: [CommonModule, RouterModule, SharedModule, ModalModule],
   declarations: [...Components],
   exports: [...ComponentsExport],
 })
