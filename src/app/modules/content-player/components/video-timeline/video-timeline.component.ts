2ed8217794c131485ec8ebd7b04fe1d295ffc1b2
mpskeeter <mpskeeter@gmail.com>
2022-05-18 14:14:41 -0400

    1 file modified

--- src/app/modules/content-player/components/video-timeline/video-timeline.component.ts
+++ src/app/modules/content-player/components/video-timeline/video-timeline.component.ts
@@ -1,11 +1,34 @@
-import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
-import { Screen } from '../../models';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { VideoDuration, Screen } from '../../models';
 
 @Component({
   selector: 'app-video-timeline',
   templateUrl: './video-timeline.component.html',
 })
 export class VideoTimelineComponent {
-  @ViewChild('timeline') timeline: HTMLElement;
-}
+  @Input() duration: Partial<VideoDuration> = {};
 
+  percent: number;
+  rightSide: string;
+
+  setEvent(event: MouseEvent) {
+    var target = event.target as HTMLElement;
+    const rect = target.getBoundingClientRect();
+
+    this.percent =
+      Math.min(Math.max(0, event.x - rect.x), rect.width) / rect.width;
+
+    this.rightSide = `before:right-[${100 - this.percent * 100}%]`;
+
+    console.log('percent:', {
+      percent: this.percent,
+      rightSide: this.rightSide,
+    });
+  }
+}
