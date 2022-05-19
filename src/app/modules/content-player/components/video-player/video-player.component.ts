3ce159ede9a4385ecc2738ef868d1189d3843a7f
Mark Peters <mpskeeter@gmail.com>
2022-05-19 11:27:45 -0400

    chore: updates

--- src/app/modules/content-player/components/video-player/video-player.component.ts
+++ src/app/modules/content-player/components/video-player/video-player.component.ts
@@ -12,7 +12,7 @@ import { DOCUMENT } from '@angular/common';
 
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
-import { Captions, Controls, VideoDuration } from '../../models';
+import { Captions, Controls, Screen, VideoDuration } from '../../models';
 import { Player } from '../../../shared-types';
 import { PlayerService } from '../../../shared';
 
@@ -20,7 +20,8 @@ import { PlayerService } from '../../../shared';
   selector: 'app-video-player',
   templateUrl: './video-player.component.html',
 })
-export class VideoPlayerComponent implements OnInit, OnDestroy, AfterViewInit {
+export class VideoPlayerComponent implements AfterViewInit {
+  // OnInit, OnDestroy, AfterViewInit {
   player: HTMLVideoElement;
 
   @ViewChild('video')
@@ -37,33 +38,30 @@ export class VideoPlayerComponent implements OnInit, OnDestroy, AfterViewInit {
     screen: { theater: false, full: false, mini: false },
   };
 
-  captions: unknown;
+  // captions: unknown;
 
-  item: Partial<Player> = {};
-  destroy$: Subject<boolean> = new Subject<boolean>();
+  // item: Partial<Player> = {};
+  // destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(
     public playerService: PlayerService,
     @Inject(DOCUMENT) private document: Document
   ) {}
 
-  ngOnInit(): void {
-    this.playerService.item$
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((item: Partial<Player>) => {
-        this.item = item;
-      });
-  }
+  // ngOnInit(): void {
+  //   this.playerService.item$
+  //     .pipe(takeUntil(this.destroy$))
+  //     .subscribe((item: Partial<Player>) => {
+  //       this.item = item;
+  //     });
+  // }
 
-  ngOnDestroy(): void {
-    this.destroy$.next(true);
-    this.destroy$.complete();
-  }
+  // ngOnDestroy(): void {
+  //   this.destroy$.next(true);
+  //   this.destroy$.complete();
+  // }
 
   ngAfterViewInit() {
-    // console.log('Values on ngAfterViewInit():');
-    console.log('video:', this.player);
-
     this.player.addEventListener('loadeddata', () => {
       this.controls.duration.totalTime = this.player.duration;
     });
@@ -94,48 +92,14 @@ export class VideoPlayerComponent implements OnInit, OnDestroy, AfterViewInit {
     };
   }
 
-  onVideoEnded() {
+  onVideoEnded(item: Partial<Player>) {
     this.playerService.setWatched({
-      userId: this.item?.user?.id,
-      courseId: this.item?.course?.id,
-      itemId: this.item?.playlistItem?.id,
-      sourceId: this.item?.source?.id,
+      userId: item?.user?.id,
+      courseId: item?.course?.id,
+      itemId: item?.playlistItem?.id,
+      sourceId: item?.source?.id,
     });
-    this.playerService.setSourceId(this.item?.source?.id + 1);
-  }
-
-  changeControls(controls: Controls) {
-    this.controls = controls;
-    controls.playing ? this.player.play() : this.player.pause();
-    this.player.currentTime = controls.duration.currentTime;
-    this.player.volume = controls.volume.volume;
-    this.player.muted = controls.volume.muted;
-    if (!controls.captions.disabled) {
-      this.player.textTracks[0].mode = controls.captions.captions
-        ? 'showing'
-        : 'hidden';
-    }
-    this.player.playbackRate = controls.speed;
-
-    console.log(
-      'this.document?.pictureInPictureElement:',
-      this.document?.pictureInPictureElement
-    );
-
-    controls.screen.mini
-      ? this.player?.requestPictureInPicture()
-      : this.document?.pictureInPictureEnabled !== null &&
-        this.document.pictureInPictureElement !== null &&
-        this.document?.exitPictureInPicture();
-
-    controls.screen.full
-      ? this.player?.requestFullscreen()
-      : this.document?.fullscreenElement !== null &&
-        this.document?.exitFullscreen();
-
-    // this.player.currentTime = this.controls.duration.currentTime;
-
-    this.controls = controls;
+    this.playerService.setSourceId(item?.source?.id + 1);
   }
 
   playPause(playing: any) {
@@ -145,18 +109,27 @@ export class VideoPlayerComponent implements OnInit, OnDestroy, AfterViewInit {
 
   setDuration(duration: Partial<VideoDuration>) {
     this.controls.duration = duration;
+    this.player.currentTime = this.controls.duration.currentTime;
   }
 
   changeVolume(volume: any) {
     this.controls.volume = volume;
+    this.player.volume = this.controls.volume.volume;
+    this.player.muted = this.controls.volume.muted;
   }
 
   setCaptions(captions: Partial<Captions>) {
     this.controls.captions = captions;
+    if (!this.controls.captions.disabled) {
+      this.player.textTracks[0].mode = this.controls.captions.captions
+        ? 'showing'
+        : 'hidden';
+    }
   }
 
   setSpeed(speed: number) {
     this.controls.speed = speed;
+    this.player.playbackRate = this.controls.speed;
   }
 
   setScreen(screen: Partial<Screen>) {
