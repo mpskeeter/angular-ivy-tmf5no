85f26cd3256ccc35e9de93939899e6ec92968fd2
Mark Peters <mpskeeter@gmail.com>
2022-05-24 12:43:20 -0400

    chore: updates

--- src/app/modules/table/directives/outside-click.directive.ts
+++ src/app/modules/table/directives/outside-click.directive.ts
@@ -2,53 +2,34 @@ import {
   Directive,
   ElementRef,
   Optional,
-  Inject,
+  HostListener,
   Output,
   EventEmitter,
   OnInit,
   OnDestroy,
 } from '@angular/core';
-import { fromEvent, Subscription } from 'rxjs';
 import { DOCUMENT } from '@angular/common';
-import { filter } from 'rxjs/operators';
 
 @Directive({
   selector: '[outsideClick]',
 })
-export class OutsideClickDirective implements OnInit, OnDestroy {
+export class OutsideClickDirective {
   @Output('outsideClick') outsideClick = new EventEmitter<MouseEvent>();
 
-  private subscription: Subscription;
-
-  constructor(
-    private element: ElementRef,
-    @Optional() @Inject(DOCUMENT) private document: any
-  ) {}
-
-  ngOnInit() {
-    setTimeout(() => {
-      this.subscription = fromEvent<MouseEvent>(this.document, 'click')
-        .pipe(
-          filter((event) => {
-            const clickTarget = event.target as HTMLElement;
-            return !this.isOrContainsClickTarget(
-              this.element.nativeElement,
-              clickTarget
-            );
-          })
-        )
-        .subscribe((event) => this.outsideClick.emit());
-    }, 0);
+  @HostListener('document:click', ['$event', '$event.target'])
+  onClick(event: MouseEvent, targetElement: HTMLElement): void {
+    console.log('event:', event);
+    console.log('targetElement:', targetElement);
+    if (!targetElement) {
+      return;
+    }
+    const clickedInside = this.elementRef.nativeElement.contains(targetElement);
+    if (!clickedInside) {
+      this.outsideClick.emit(event);
+    }
   }
 
-  private isOrContainsClickTarget(
-    element: HTMLElement,
-    clickTarget: HTMLElement
-  ) {
-    return element == clickTarget || element.contains(clickTarget);
-  }
-
-  ngOnDestroy() {
-    if (this.subscription) this.subscription.unsubscribe();
+  constructor(private elementRef: ElementRef) {
+    console.log('elementRef:', this.elementRef);
   }
 }
