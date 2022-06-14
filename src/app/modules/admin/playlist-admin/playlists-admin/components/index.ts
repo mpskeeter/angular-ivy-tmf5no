df9f6f321015f34dffcd9cad9db5ff5105727e23
Mark Peters <mpskeeter@gmail.com>
2022-06-14 09:46:08 -0400

    chore: breaking out components for multi-use

--- src/app/modules/admin/playlist-admin/playlists-admin/components/index.ts
+++ src/app/modules/admin/playlist-admin/playlists-admin/components/index.ts
@@ -1,19 +1,22 @@
+import { LayoutComponent } from './layout';
 import { PlaylistsEditComponent } from './playlists-edit';
 import { PlaylistsBuildComponent } from './playlists-build';
 import { PlaylistsTableComponent } from './playlists-table';
-import { LayoutComponent } from './layout';
+import { SaveCancelButtonsComponent } from './save-cancel-buttons';
 
 export const ComponentsExport = [];
 
 export const Components = [
   ...ComponentsExport,
+  LayoutComponent,
   PlaylistsEditComponent,
   PlaylistsBuildComponent,
   PlaylistsTableComponent,
-  LayoutComponent,
+  SaveCancelButtonsComponent,
 ];
 
+export * from './layout';
 export * from './playlists-edit';
 export * from './playlists-build';
 export * from './playlists-table';
-export * from './layout';
+export * from './save-cancel-buttons';
---
+++ src/app/modules/admin/playlist-admin/playlists-admin/components/save-cancel-buttons/save-cancel-buttons.component.ts
@@ -0,0 +1,11 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'save-cancel-buttons',
+  templateUrl: './save-cancel-buttons.component.html',
+})
+export class SaveCancelButtonsComponent {
+  @Input() disabled: boolean;
+  @Output() save: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() cancel: EventEmitter<boolean> = new EventEmitter<boolean>();
+}
