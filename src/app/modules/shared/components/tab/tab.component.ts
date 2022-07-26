8b3dbb91bfb1f2c0ef1320435b03a40b9cef386f
Mark Peters <mpskeeter@gmail.com>
2022-07-26 14:08:27 -0400

    chore: changes

--- src/app/modules/shared/components/tab/tab.component.ts
+++ src/app/modules/shared/components/tab/tab.component.ts
@@ -2,7 +2,8 @@ import {
   ChangeDetectionStrategy,
   Component,
   Input,
-  OnInit,
+  OnChanges,
+  SimpleChanges,
 } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
@@ -15,30 +16,18 @@ import { NavbarItem } from '../../../shared-types';
   styleUrls: ['./tab.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TabComponent implements OnInit {
+export class TabComponent implements OnChanges {
   @Input() menu: Partial<NavbarItem>[] = [];
   @Input() activeLink: Partial<NavbarItem> =
     this.menu.length > 0 ? this.menu[0] : {};
 
-  // url: string = '';
+  constructor(private route: Router) {}
 
-  constructor(private route: Router) {
-    // this.route.url.pipe(
-    //   map((segments) => {
-    //     this.url = segments.join('');
-    //   })
-    // );
-
-    console.log('this.route.url:', this.route.url);
-    this.activeLink = this.menu?.find((item: Partial<NavbarItem>) => {
-      console.log('item:', item);
-
-      return this.route.url.includes(item.link);
-    });
-  }
-
-  ngOnInit() {
-    // console.log('route:', this.route);
-    console.log('activeLink:', this.activeLink);
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.menu) {
+      this.activeLink = this.menu?.find((item: Partial<NavbarItem>) => {
+        return this.route.url.includes(item.link);
+      });
+    }
   }
 }
