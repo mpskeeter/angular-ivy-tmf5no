840887a1a2a45d49d5a0201d1eab70df0257f9ba
Mark Peters <mpskeeter@gmail.com>
2022-07-26 09:32:03 -0400

    chore(material): updates

--- src/app/modules/shared/components/tab/tab.component.ts
+++ src/app/modules/shared/components/tab/tab.component.ts
@@ -4,9 +4,9 @@ import { NavbarItem } from '../../../shared-types';
 @Component({
   selector: 'app-tab',
   templateUrl: './tab.component.html',
+	styleUrls: ['./tab.component.scss'],
 })
 export class TabComponent {
   @Input() menu: Partial<NavbarItem>[] = [];
-
-  activeLink = this.menu[0];
+  @Input() activeLink: Partial<NavbarItem> = this.menu[0];
 }
