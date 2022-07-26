2a3c5e21f29cacfcac7aac5656262ea418fd93fa
Mark Peters <mpskeeter@gmail.com>
2022-07-26 11:24:25 -0400

    chore: update

--- src/app/modules/shared/components/tab/tab.component.ts
+++ src/app/modules/shared/components/tab/tab.component.ts
@@ -1,12 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { NavbarItem } from '../../../shared-types';
 
 @Component({
   selector: 'app-tab',
   templateUrl: './tab.component.html',
-	styleUrls: ['./tab.component.scss'],
+  styleUrls: ['./tab.component.scss'],
 })
-export class TabComponent {
+export class TabComponent implements OnInit {
   @Input() menu: Partial<NavbarItem>[] = [];
-  @Input() activeLink: Partial<NavbarItem> = this.menu[0];
+  @Input() activeLink: Partial<NavbarItem> =
+    this.menu.length > 0 ? this.menu[0] : {};
+
+  constructor(private route: ActivatedRoute) {
+    const url: Observable<string> = route.url.pipe(
+      map((segments) => segments.join(''))
+    );
+
+    console.log('url:', url);
+  }
+
+  ngOnInit() {
+    console.log('route:', this.route);
+    console.log('activeLink:', this.activeLink);
+  }
 }
