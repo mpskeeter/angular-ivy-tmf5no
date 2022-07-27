395713dfaeed18651264af282a6e5e9fa1a04c39
Mark Peters <mpskeeter@gmail.com>
2022-07-27 12:21:43 -0400

    chore: add MatIcon

--- src/app/modules/shared/material.module.ts
+++ src/app/modules/shared/material.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -11,6 +12,7 @@ const ModulesExport = [
   CommonModule,
   DragDropModule,
   MatButtonModule,
+  MatIconModule,
   MatMenuModule,
   MatTableModule,
   MatTabsModule,
